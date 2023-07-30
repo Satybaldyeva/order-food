0fe85c7d0945dde11207ea8891419bab5c01ac12
Kanykei <kanisatybaldyeva@gmail.com>
2023-07-30 17:11:31 +0600

    add mealssummary component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Header } from "./components/header/Header";
+import { MealsSummary } from "./components/meals-summary/MealsSummary";
 
 function App() {
   return (
     <div >
       <Header/>
+      <MealsSummary/>
       {/* <Header/>
       <MealsSummary/>
       <Meals/>
       <MealItem/> */}
       
-    </div>
+    </div> 
   );
 }
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,4 @@
-export {ReactComponent as BasketIcon} from './icons/basket-icon.svg'
+export {ReactComponent as BasketIcon} from './icons/basket-icon.svg';
+
+
+export {default as MealsBannerImage} from './images/meals-image.png'
