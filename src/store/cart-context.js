d7d9d428341a218a723cc4580cb5240545def826
Kanykei <kanisatybaldyeva@gmail.com>
2023-08-04 09:05:11 +0600

    added total amount

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,30 +1,141 @@
-import React, {useState} from 'react'
+import React, {useReducer} from 'react'
 
 export const CartContext = React.createContext({
     addedMeals: [],
     totalAmount: 0,
     onAddMeal: ()=>{},
+    onIncreaseMealsAmount: ()=>{},
+    onDecreaseMealsAmountHandler: ()=>{},
 })
 
-export const CartProvider = ({children})=>{
-    const [addedMeals, setAddedMeals] = useState([])
 
-    const addMealsHandler = (newMeal)=>{
-        const currentIndex = addedMeals.findIndex((m)=> m.id === newMeal.id)
-        if (currentIndex === -1) {
-            return setAddedMeals([...addedMeals, newMeal])
-        }
-        const newMeals = addedMeals.map((meal)=>{
-            if (meal.id === newMeal.id) {
+const ADD_MEAL_TYPE = 'ADD_MEAL';
+const INCREASE_MEAL_AMOUNT = 'INCREASE-AMOUNT'
+const DECREASE_MEAL_AMOUNT = 'DECREASE-AMOUNT'
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case ADD_MEAL_TYPE: {
+            const prevMeals = state.addedMeals
+            const newMeal= action.payload
+
+            if(prevMeals.lenght === 0) {
                 return {
-                    ...meal, 
-                    amount: meal.amount +newMeal.amount, 
+                    ...state,
+                    addedMeals: [newMeal],
                 }
             }
-            return meal
-        })
 
-        setAddedMeals(newMeals)
+            const isMealExists = prevMeals.find((meal)=>meal.id === newMeal.id)
+
+            if (isMealExists === undefined) {
+                return {
+                    ...state, 
+                    addedMeals: [...prevMeals, newMeal],
+                }
+            }
+
+            const newAddedMeals = prevMeals.map((meal)=> {
+                if(meal.id===newMeal.id) {
+                    return{
+                        ...meal,
+                        amount:meal.amount + newMeal.amount
+                    }
+                  
+                } 
+                return meal
+               
+            })
+            return {
+                ...state,
+                addedMeals: newAddedMeals,
+            }
+        }
+
+        case INCREASE_MEAL_AMOUNT: {
+            const prevMeals = state.addedMeals
+            const mealId = action.payload
+            const newAddedMeals = prevMeals.map((meal)=>{
+                if(meal.id ===mealId) {
+                    return {
+                        ...meal,
+                        amount: meal.amount +1
+                    }
+                   
+                }
+                return meal
+
+            })
+
+            return {
+                ...state,
+                addedMeals: newAddedMeals,
+            }
+        }
+
+        case DECREASE_MEAL_AMOUNT: {
+            const prevMeals = state.addedMeals
+            const mealId = action.payload
+            const currentMealItem = prevMeals.find((meal)=> meal.id===mealId)
+
+            if(currentMealItem.amount ===1 ) {
+                return {
+                    ...state, 
+                    addedMeals: prevMeals.filter((meal)=>meal.id !== currentMealItem.id),
+
+                }
+            }
+            const newAddedMeals = prevMeals.map((meal)=>{
+                if(meal.id === mealId) {
+                    return {
+                        ...meal,
+                        amount: meal.amount -1
+                    }
+
+                    
+                }
+                return meal
+            })
+
+            return {
+                ...state,
+                addedMeals: newAddedMeals,
+            }
+        }
+            
+          
+    
+        default:
+           return state
+    }
+}
+
+export const CartProvider = ({children})=>{
+    // const [addedMeals, setAddedMeals] = useState([])
+
+    const [state, dispatch] = useReducer(reducer, {addedMeals:[]})
+
+    const {addedMeals=[]}=state
+
+    const totalAmount = addedMeals.reduce((acc, meal)=> {
+        return acc+meal.price * meal.amount
+    }, 0)
+
+    console.log(totalAmount);
+
+    const addMealsHandler = (newMeal)=>{
+        dispatch({type:ADD_MEAL_TYPE, payload: newMeal})
+    }
+
+    const increaseMealAmountHandler = (id)=>{
+        console.log(id);
+        dispatch({type: INCREASE_MEAL_AMOUNT, payload: id})
+    }
+
+    const decreaseMealAmountHandler = (id)=>{
+        console.log(id);
+        dispatch({type: DECREASE_MEAL_AMOUNT, payload: id})
     }
 
     return (
@@ -32,6 +143,8 @@ export const CartProvider = ({children})=>{
             addedMeals, 
             onAddMeal: addMealsHandler, 
             totalAmount: 0,
+            onIncreaseMealsAmount: increaseMealAmountHandler,
+            onDecreaseMealsAmountHandler:decreaseMealAmountHandler,
         }}>
             {children}
         </CartContext.Provider>
